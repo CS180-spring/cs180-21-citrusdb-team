0c9726c780ab3d62184aa8de96f65e3275fe2c8e
EC2 Default User <ec2-user@ip-172-31-14-155.us-east-2.compute.internal>
2023-05-26 05:40:44 +0000

    Added/Updated Files for Testing

--- frontend/citrusdb/src/components/API/main.cpp
+++ frontend/citrusdb/src/components/API/main.cpp
@@ -1,20 +1,15 @@
-// To Compile
-// g++ main.cpp -I/home/linuxbrew/.linuxbrew/opt/asio/include -L/home/linuxbrew/.linuxbrew/lib -lboost_system
-
 #include "crow.h"
 
-using namespace std;
+int main()
+{
+    crow::SimpleApp app;
 
-int main() {
-    crow::SimpleApp app; 
-
-    
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/<string>")([](std::string name){ 
+        auto page = crow::mustache::load("test.html"); 
+        crow::mustache::context ctx ({{"person", name}}); 
+        return page.render(ctx); 
     });
 
-    //set the port, set the app to run on multiple threads, and run the app
-    cout << "Listening on port 8080;" << endl;
     app.port(8080).multithreaded().run();
-    return 0;
 }
---
+++ frontend/citrusdb/src/components/API/templates/test.html
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+  <body>
+  <p>Hello {{person}}!</p> <!---->
+  </body>
+</html>
